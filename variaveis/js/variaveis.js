33222c1aedc97b6d59f22f8be3c0b57050b9d726
JadiellyL <desenvolvedorajadielly@gmail.com>
2022-05-31 12:03:34 -0300

    organizando pasta

---
+++ variaveis/js/variaveis.js
@@ -0,0 +1,19 @@
+let nome = "joao"//declaração mais recente de variavel. Aqui estamos declarando e inicializando em apenas uma linha
+//var forma de declarar variavel mais antigo
+let idade; //sem declarar valor é mostrado uma resposta que nos indica que a variavel nao tem um valor no momento
+//js é escrito da esquerda pra direita e de cima para baixo
+//javascript é uma linguagem de tipgem dinamica
+
+console.log(nome, "esta feliz");
+
+//regras para criar variaveis
+
+//não se pode criar variaveis com palavras resrvadas
+//precisam ter nomes significativos
+//não pode comecar nome de variavel com numero
+//não podem conter espaços
+//utiliza-se camelcase
+//sao case sensitive
+//no podemos redeclarar variaveis com let
+
+
