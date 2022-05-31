8da2f02bc14f855ef5149cca548f2a88ae9e5c5c
JadiellyL <desenvolvedorajadielly@gmail.com>
2022-05-31 12:48:15 -0300

    estilizando paginas

---
+++ constantes/js/constantes.js
@@ -0,0 +1,18 @@
+//regras para criar constantes
+
+//não se pode criar constantes com palavras resrvadas
+//precisam ter nomes significativos
+//não pode comecar nome de constantes com numero
+//não podem conter espaços
+//utiliza-se camelcase
+//sao case sensitive
+//no podemos modificar o valor de uma constante
+//nao utilize var, utilize const
+const nome = "Joao";//deve ser criada e inicilizada
+console.log(nome);
+const num = 10;
+const num2 = 2;
+let resultado = num + num2;
+console.log(resultado);
+
+
