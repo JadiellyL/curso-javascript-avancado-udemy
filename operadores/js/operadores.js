2755a4886963b119009b5e42c1d41a730bff8eab
JadiellyL <desenvolvedorajadielly@gmail.com>
2022-06-01 19:02:47 -0300

    Operadores

---
+++ operadores/js/operadores.js
@@ -0,0 +1,28 @@
+//OPERADORES ARITMETICOS, DE ATRIBUIÇÃO E DE INCREMENTO
+/*
+OPeradores aritmeticos:
++ adição/concatenação
+- / *
+** potenciação
+% modeo da divisão, retorna o resto da divisão
+() precedencia
+---------------------------------
+ordem de precedencia dos operadores aritmeticos
+()
+**
+* / %
++ -
+*/
+
+/*OPERADOR DE INCREMENTO E DECREMENTO - incrementa e decrementa de 1 em um
+++ 
+--
+*/
+let cont = 1;
+cont++; //executa a acao e depois mostra o valor a partir da linha seguinte
+++cont;//primeiro faz a conta e depois retorna o valor na mesma linha
+console.log(cont);
+/*
+OPERADOR DE ATRIBUIÇÃO
+=
+*/
