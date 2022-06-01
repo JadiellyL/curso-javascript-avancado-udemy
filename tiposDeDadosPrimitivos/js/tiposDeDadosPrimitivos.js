209a7fec47ae3c57461c5f11408e2ddd5b26d480
JadiellyL <desenvolvedorajadielly@gmail.com>
2022-06-01 18:17:28 -0300

    Tipos de dados primitivos

---
+++ tiposDeDadosPrimitivos/js/tiposDeDadosPrimitivos.js
@@ -0,0 +1,9 @@
+//TIPOS DE DADOS PRIMITIVOS
+const nome = "Jadielly";//string
+let numero = 10;//number
+let numero2 = 10.2;//number
+let nomeAluno; //undefined = não aponta para local nenhum na memoria, esse valor indica que a variavel ainda não tem valor 
+let sobrenomeAluno = null; //não aponta para lugar nenhum na memoria, utiliza-se quando queremos colocar o valor de uma variavel como null
+let boolean = true;//seu valor e true ou false, valor lógico
+
+console.log(typeof nome);//para ver o tipo da variavel
